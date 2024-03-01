c095fd49fbab96638cef1df9f4bf2adf73a18b2e
ankit-K2 <ankitkumargre1904@gmail.com>
2024-03-01 15:21:14 +0530

    first commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { MdSearch } from "react-icons/md";
 
-const reportWebVitals = ({ handleSearchNote }) => {
+const Search = ({ handleSearchNote }) => {
   return (
-    <div>
+    <div className="search">
+      <MdSearch className="search-icons" size="1.3em" />
       <input
         onChange={(event) => handleSearchNote(event.target.value)}
         type="text"
@@ -12,4 +14,4 @@ const reportWebVitals = ({ handleSearchNote }) => {
   );
 };
 
-export default reportWebVitals;
+export default Search;
